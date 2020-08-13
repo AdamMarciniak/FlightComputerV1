d4e5c0568c399f8b74738df6da3989c40a4ddad7
Adam <adam668@gmail.com>
2020-08-13 09:16:28 -0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,26 +1,3 @@
-
-enum state {
-  PAD_IDLE,
-  IGNITION_FIRING,
-  POWERED_ASCENT,
-  UNPOWERED_ASCENT,
-  DESCENT,
-  PARACHUTE_FIRING,
-  PARACHUTE_DESCENT,
-  LANDED,
-}
-
-String stateStrings[] {
-  "PAD_IDLE",
-  "IGNITION_FIRING",
-  "POWERED_ASCENT",
-  "UNPOWERED_ASCENT",
-  "DESCENT",
-  "PARACHUTE_FIRING",
-  "PARACHUTE_DESCENT",
-  "LANDED",
-}
-
 #define TVC_X_PIN 2
 #define TVC_Y_PIN 3
 #define IMU_INTERRUPT_PIN 14
@@ -31,18 +8,79 @@ String stateStrings[] {
 #define PYRO_2_FIRE_PIN 6
 
 
+enum state {
+  PAD_IDLE,
+  IGNITION_FIRING,
+  POWERED_ASCENT,
+  UNPOWERED_ASCENT,
+  DESCENT,
+  PARACHUTE_FIRING,
+  PARACHUTE_DESCENT,
+  LANDED,
+  FAILURE
+};
+
+String stateStrings[] {
+  "PAD_IDLE",
+  "IGNITION_FIRING",
+  "POWERED_ASCENT",
+  "UNPOWERED_ASCENT",
+  "DESCENT",
+  "PARACHUTE_FIRING",
+  "PARACHUTE_DESCENT",
+  "LANDED",
+  "FAILURE"
+};
+
+state currentState = PAD_IDLE;
+
+unsigned long currentMillis;
+unsigned long previousMillis;
 
 void setup() {
 
 }
 
+
+
 void loop() {
 
+  switch(currentState) {
+    case PAD_IDLE: {
+      break;
+    }
 
+    case IGNITION_FIRING: {
+      break;
+    }
 
+    case POWERED_ASCENT: {
+      break;
+    }
 
+    case UNPOWERED_ASCENT: {
+      break;
+    }
 
+    case DESCENT: {
+      break;
+    }
 
-  
+    case PARACHUTE_FIRING: {
+      break;
+    }
+
+    case PARACHUTE_DESCENT: {
+      break;
+    }
+    
+    case LANDED: {
+      break;
+    }
+    
+    case FAILURE: {
+      break;
+    }
+  }
 
 }
