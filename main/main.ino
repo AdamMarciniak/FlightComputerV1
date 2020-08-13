58ead1902c6e775926b766fa36dfa6860ae4980c
Adam <adam668@gmail.com>
2020-08-13 09:02:56 -0700

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,48 @@
+
+enum state {
+  PAD_IDLE,
+  IGNITION_FIRING,
+  POWERED_ASCENT,
+  UNPOWERED_ASCENT,
+  DESCENT,
+  PARACHUTE_FIRING,
+  PARACHUTE_DESCENT,
+  LANDED,
+}
+
+String stateStrings[] {
+  "PAD_IDLE",
+  "IGNITION_FIRING",
+  "POWERED_ASCENT",
+  "UNPOWERED_ASCENT",
+  "DESCENT",
+  "PARACHUTE_FIRING",
+  "PARACHUTE_DESCENT",
+  "LANDED",
+}
+
+#define TVC_X_PIN 2
+#define TVC_Y_PIN 3
+#define IMU_INTERRUPT_PIN 14
+#define BUZZER_PIN 8
+#define PYRO_1_DETECT_PIN 23
+#define PYRO_2_DETECT_PIN 22
+#define PYRO_1_FIRE_PIN 5
+#define PYRO_2_FIRE_PIN 6
+
+
+
+void setup() {
+
+}
+
+void loop() {
+
+
+
+
+
+
+  
+
+}
